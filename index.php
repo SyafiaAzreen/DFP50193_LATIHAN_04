afeeb82aaf1d4bbde9aeed85e1fa453fe3cb1426
SyafiaAzreen <syafiaazreen323@gmail.com>
2021-10-07 08:26:39 +0800

    constants yang simple

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$jawatan = "Pengurus";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constants</title>
+    <style>
+        p {
+            font-size: 24px;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <p>Saya, selaku <?php echo $jawatan; ?>
+        dengan ini melantik Encik ahmad sebagai
+        Penolong <?php echo $jawatan; ?>.
+    </p>
+</body>
+
+</html>
